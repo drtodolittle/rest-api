714b2c1b262a3c97eae8eb102f740c35016cfb6f
Dirk Günther <d.guenther@cenit.de>
2015-12-18 00:09:13 +0100

    von Script auf Processor umgestellt. Die Groovy Scripte werden jetzt erst nach Java compiliert

---
+++ src/main/groovy/de/drtodolittle/processor/ExtractProcessInstanceIds.groovy
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.drtodolittle.processor
+
+import groovy.json.JsonSlurper
+import org.apache.camel.Exchange
+import org.apache.camel.Processor
+
+/**
+ *
+ * @author Guenther_D
+ */
+class ExtractProcessInstanceIds implements Processor {
+	
+     public void process(Exchange exchange) {
+        dev input = exchange.getIn().getBody()
+        def jsonSlurper = new JsonSlurper()
+        def tasks = jsonSlurper.parseText(input)
+        def processIds = []
+
+        tasks.each({processIds.add(it.id)})
+
+        exchange.getIn().setHeader("processIds", processIds)
+         
+     }
+}
+

9f5786dc91a5b72dd5f282ffd3b922fece3376b6
Dirk Günther <d.guenther@cenit.de>
2015-12-19 20:59:53 +0100

    GenerateToDoJSON hinzugefügt

---
+++ src/main/groovy/de/drtodolittle/processor/GenerateToDoJSON.groovy
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.drtodolittle.processor
+
+import groovy.json.JsonSlurper
+import org.apache.camel.Exchange
+import org.apache.camel.Processor
+
+/**
+ *
+ * @author Guenther_D
+ */
+class GenerateToDoJSON implements Processor {
+	
+     public void process(Exchange exchange) {
+        def input = exchange.in.body
+        def jsonSlurper = new JsonSlurper()
+        def vars = jsonSlurper.parse(input)
+        
+        def todo = [:]
+        
+        todo.put("id", exchange.in.headers.processIds[exchange.properties.CamelSplitIndex])
+        
+        vars.each({
+                if (it.name.equals("topic")) {
+                    todo.put("topic", it.value)
+                }
+                else if (it.name.equals("done")) {
+                    todo.put("done", it.value)
+                }
+            })
+        exchange.in.body = JsonOutput.toJson(todo)
+     }
+}
+

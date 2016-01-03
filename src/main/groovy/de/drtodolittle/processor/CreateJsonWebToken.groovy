b41816ceccaf515a8eadc52dfda2c433e2718935
Dirk Günther <d.guenther@cenit.de>
2016-01-03 19:14:53 +0100

    ISSUE 16: Anmeldung

---
+++ src/main/groovy/de/drtodolittle/processor/CreateJsonWebToken.groovy
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package de.drtodolittle.processor
+
+import java.security.Key
+
+import javax.crypto.spec.SecretKeySpec
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+
+/**
+ * @author Guenther_D
+ *
+ */
+public class CreateJsonWebToken implements Processor {
+
+	
+	private String secret = "camundaunddrtodolittlesecret"
+	
+	
+	/* (non-Javadoc)
+	 * @see org.apache.camel.Processor#process(org.apache.camel.Exchange)
+	 */
+	
+	public void process(Exchange exchange) throws Exception {
+		String user = (String) exchange.getIn().getHeader("Username")
+		Key key = new SecretKeySpec(secret.getBytes(), SignatureAlgorithm.ES256.getJcaName())
+		String token = Jwts.builder().setSubject(user).signWith(SignatureAlgorithm.HS512, key).compact()
+		exchange.getIn().setBody("{'token':'" + token + "'}")
+	}
+
+}
---
+++ src/test/java/de/drtodolittle/processor/CreateJsonWebTokenTest.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package de.drtodolittle.processor;
+
+import java.security.Key;
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+
+/**
+ * @author Guenther_D
+ *
+ */
+public class CreateJsonWebTokenTest {
+
+	@Test
+	public void testCreateToken() {
+		Key key = new SecretKeySpec("dirk".getBytes(), SignatureAlgorithm.ES256.getJcaName());
+		
+		String s = Jwts.builder().setSubject("dirk").signWith(SignatureAlgorithm.HS512, key).compact();
+		System.out.println(s);
+	}
+
+	@Test
+	public void testVerifyToken() {
+		Key key = new SecretKeySpec("dirk".getBytes(), SignatureAlgorithm.ES256.getJcaName());
+		
+		String s = Jwts.builder().setSubject("dirk").signWith(SignatureAlgorithm.HS512, key).compact();
+		System.out.println(s);
+		try {
+
+		    Jws<Claims> parseClaimsJws = Jwts.parser().setSigningKey(key).parseClaimsJws(s);
+		    Assert.assertEquals("dirk", parseClaimsJws.getBody().getSubject());
+
+		    System.out.println("Alles gut");
+
+		} catch (SignatureException e) {
+
+		    System.out.println("Hier stimmt was nicht");
+		}
+	}
+}

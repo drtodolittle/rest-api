e21d6126608b99309256863d05f8e8eec590779f
Dirk Günther <d.guenther@cenit.de>
2016-01-05 23:40:49 +0100

    Modify ToDo umgesetzt

---
+++ src/main/groovy/de/drtodolittle/processor/ExtractTopic.groovy
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.drtodolittle.processor
+
+import groovy.json.JsonSlurper
+import org.apache.camel.Exchange
+import org.apache.camel.Processor
+
+/**
+ *
+ * @author Guenther_D
+ */
+class ExtractTopic implements Processor {
+
+     public void process(Exchange exchange) {
+        def input = exchange.getIn().getBody()
+        def jsonSlurper = new JsonSlurper()
+        def task = jsonSlurper.parse(input)
+        exchange.getIn().setBody('{"value" : "' + task.topic + '", "type": "String"}')
+     }
+}

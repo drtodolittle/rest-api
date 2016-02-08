2687a035a424c5480f6d5263cabe101163341ae2
Dirk Günther <d.guenther@cenit.de>
2016-02-08 20:59:19 +0100

    User Registration Rest Service um das Starten des Camunda Processes erweitert

---
+++ src/main/groovy/de/drtodolittle/processor/GenerateCreateProcessUserRegistryJSON.groovy
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.drtodolittle.processor
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+import org.apache.camel.Exchange
+import org.apache.camel.Processor
+
+/**
+ *
+ * @author Guenther_D
+ */
+class GenerateCreateProcessUserRegistryJSON implements Processor{
+
+
+
+    public void process(Exchange exchange) {
+       def input = exchange.in.body
+       def username = exchange.in.headers.username
+       def jsonSlurper = new JsonSlurper()
+       def vars = jsonSlurper.parse(input)
+
+
+
+        def builder = new groovy.json.JsonBuilder()
+        builder {
+            variables {
+                email {
+                    value vars.email
+                    type 'String'
+				}
+				firstname {
+                    value vars.firstname
+                    type 'String'
+				}
+				lastname {
+                    value vars.lastname
+                    type 'String'
+				}
+				password {
+                    value vars.password
+                    type 'String'
+				}
+
+            }
+            businessKey vars.email
+	}
+
+        exchange.in.body = builder.toString()
+     }
+
+
+
+
+
+
+}

0ef5436dbea1003f92a62235501ff606cb988546
Dirk Günther <d.guenther@cloud57.de>
2017-06-05 22:26:51 +0200

    Fehler im Statup Script gefixt

--- docker/src/main/resources/start-rest-api.sh
+++ docker/src/main/resources/start-rest-api.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ "x${CAMUNDA_URI}" != "x" ]; then
-  echo "camunda_uri=${CAMUNDA_URI}" >> /opt/karaf/etc/drtodolittle.cfg
+  echo "camunda-uri=${CAMUNDA_URI}" >> /opt/karaf/etc/drtodolittle.cfg
 fi
 /opt/karaf/bin/karaf daemon
